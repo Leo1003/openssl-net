bd4d68a38322a337a947d9256e2560ede7047f6f
Leo <leo881003@gmail.com>
2018-10-10 15:32:35 +0800

    move ERR region

--- ManagedOpenSsl/Native/NativeMethods.cs
+++ ManagedOpenSsl/Native/NativeMethods.cs
@@ -1737,34 +1737,6 @@ namespace OpenSSL.Native
 
         #endregion
 
-        #region ERR
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static uint ERR_get_error();
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void ERR_error_string_n(uint e, byte[] buf, int len);
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static IntPtr ERR_lib_error_string(uint e);
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static IntPtr ERR_func_error_string(uint e);
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static IntPtr ERR_reason_error_string(uint e);
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void ERR_remove_thread_state(IntPtr tid);
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void ERR_clear_error();
-
-        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void ERR_print_errors_cb(err_cb cb, IntPtr u);
-
-        #endregion
-
         #region NCONF
 
         [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
