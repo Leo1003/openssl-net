d65b0e9b224e7be97b18cf45909124eccf232555
Leo <leo881003@gmail.com>
2018-10-12 00:45:42 +0800

    move X509 related regions

---
+++ ManagedOpenSsl/Native/Methods/x509.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OpenSSL.Native
+{
+    internal partial class NativeMethods
+    {
+        #region X509
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_dup(IntPtr x509);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_cmp(IntPtr a, IntPtr b);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_sign(IntPtr x, IntPtr pkey, IntPtr md);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_check_private_key(IntPtr x509, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_verify(IntPtr x, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_pubkey_digest(IntPtr data, IntPtr type, byte[] md, ref uint len);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_digest(IntPtr data, IntPtr type, byte[] md, ref uint len);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set_version(IntPtr x, int version);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set_serialNumber(IntPtr x, IntPtr serial);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_get_serialNumber(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set_issuer_name(IntPtr x, IntPtr name);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_get_issuer_name(IntPtr a);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set_subject_name(IntPtr x, IntPtr name);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_get_subject_name(IntPtr a);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set1_notBefore(IntPtr x, IntPtr tm);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set1_notAfter(IntPtr x, IntPtr tm);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_set_pubkey(IntPtr x, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_get_pubkey(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_free(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_verify_cert(IntPtr ctx);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_verify_cert_error_string(int n);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_to_X509_REQ(IntPtr x, IntPtr pkey, IntPtr md);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_print_ex(IntPtr bp, IntPtr x, uint nmflag, uint cflag);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_print(IntPtr bp, IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_find_by_issuer_and_serial(IntPtr sk, IntPtr name, IntPtr serial);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_find_by_subject(IntPtr sk, IntPtr name);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_check_trust(IntPtr x, int id, int flags);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_time_adj(IntPtr s, int adj, ref long t);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_gmtime_adj(IntPtr s, int adj);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr d2i_X509_bio(IntPtr bp, ref IntPtr x509);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int i2d_X509_bio(IntPtr bp, IntPtr x509);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_PUBKEY_free(IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_OBJECT_up_ref_count(IntPtr a);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_OBJECT_free(IntPtr a);
+
+        #endregion
+
+        #region X509_REQ
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_REQ_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_REQ_free(IntPtr a);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_set_version(IntPtr x, int version);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_set_pubkey(IntPtr x, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_REQ_get_pubkey(IntPtr req);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_set_subject_name(IntPtr x, IntPtr name);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_sign(IntPtr x, IntPtr pkey, IntPtr md);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_verify(IntPtr x, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_digest(IntPtr data, IntPtr type, byte[] md, ref uint len);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_REQ_to_X509(IntPtr r, int days, IntPtr pkey);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_print_ex(IntPtr bp, IntPtr x, uint nmflag, uint cflag);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_REQ_print(IntPtr bp, IntPtr x);
+
+        #endregion
+
+        #region X509_EXTENSION
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_EXTENSION_free(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_dup(IntPtr ex);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_add_ext(IntPtr x, IntPtr ex, int loc);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_add1_ext_i2d(IntPtr x, int nid, byte[] value, int crit, uint flags);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_create_by_OBJ(ref IntPtr ex, IntPtr obj, int crit, IntPtr data);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_create_by_NID(ref IntPtr ex, int nid, int crit, IntPtr data);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_EXTENSION_set_object(IntPtr ex, IntPtr obj);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_EXTENSION_set_critical(IntPtr ex, int crit);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_EXTENSION_set_data(IntPtr ex, IntPtr data);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_get_object(IntPtr ex);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_EXTENSION_get_data(IntPtr ne);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_EXTENSION_get_critical(IntPtr ex);
+
+        #endregion
+
+        #region X509_STORE
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_STORE_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_STORE_add_cert(IntPtr ctx, IntPtr x);
+
+        //[DllImport(DLLNAME, CallingConvention=CallingConvention.Cdecl)]
+        //void X509_STORE_set_flags();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_STORE_free(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_STORE_up_ref(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_STORE_CTX_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_STORE_CTX_init(IntPtr ctx, IntPtr store, IntPtr x509, IntPtr chain);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_STORE_CTX_free(IntPtr x);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_STORE_CTX_get_current_cert(IntPtr x509_store_ctx);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_STORE_CTX_get_error_depth(IntPtr x509_store_ctx);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_STORE_CTX_get0_store(IntPtr ctx);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_STORE_CTX_get_error(IntPtr x509_store_ctx);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_STORE_CTX_set_error(IntPtr x509_store_ctx, int error);
+
+        #endregion
+
+        #region X509_INFO
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_INFO_free(IntPtr a);
+
+        #endregion
+
+        #region X509_NAME
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_NAME_new();
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509_NAME_free(IntPtr a);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_NAME_dup(IntPtr xn);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_cmp(IntPtr a, IntPtr b);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_entry_count(IntPtr name);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_add_entry_by_NID(
+            IntPtr name,
+            int nid,
+            int type,
+            byte[] bytes,
+            int len,
+            int loc,
+            int set);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_add_entry_by_txt(
+            IntPtr name,
+            byte[] field,
+            int type,
+            byte[] bytes,
+            int len,
+            int loc,
+            int set);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_get_text_by_NID(IntPtr name, int nid, byte[] buf, int len);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_NAME_get_entry(IntPtr name, int loc);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_NAME_delete_entry(IntPtr name, int loc);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_get_index_by_NID(IntPtr name, int nid, int lastpos);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_digest(IntPtr data, IntPtr type, byte[] md, ref uint len);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509_NAME_oneline(IntPtr a, byte[] buf, int size);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_print(IntPtr bp, IntPtr name, int obase);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509_NAME_print_ex(IntPtr bp, IntPtr nm, int indent, uint flags);
+
+        #endregion
+    }
+}
---
+++ ManagedOpenSsl/Native/Methods/x509v3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OpenSSL.Native
+{
+    internal partial class NativeMethods
+    {
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509V3_set_ctx(
+            IntPtr ctx,
+            IntPtr issuer,
+            IntPtr subject,
+            IntPtr req,
+            IntPtr crl,
+            int flags);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void X509V3_set_nconf(IntPtr ctx, IntPtr conf);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509V3_EXT_add_nconf(IntPtr conf, IntPtr ctx, byte[] section, IntPtr cert);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int X509V3_EXT_print(IntPtr bio, IntPtr ext, uint flag, int indent);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509V3_EXT_get_nid(int nid);
+
+        [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr X509V3_EXT_conf_nid(IntPtr conf, IntPtr ctx, int ext_nid, string value);
+    }
+}
